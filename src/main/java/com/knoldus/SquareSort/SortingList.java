2ada1d9026361b1f7f5e047f051524796129f3ed
Shubham Rawat <shubham.singh@knoldus.com>
2022-03-03 11:40:19 +0530

    Initial commit

---
+++ src/main/java/com/knoldus/SquareSort/SortingList.java
@@ -0,0 +1,13 @@
+package com.knoldus.SquareSort;
+
+//import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingList {
+    static List sortList(List<Integer> list){
+        // sorting
+        Collections.sort(list);
+        return list;
+    }
+}
